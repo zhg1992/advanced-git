10c6c59b96228fa0915ee13cf6821d3ddd390cd3
Bo Zhang <zhangbodut@gmail.com>
2019-10-18 20:01:56 +0800

    Version 7.2

--- Product.java
+++ Product.java
@@ -1,6 +1,10 @@
 public class Product {
     public static void main(String[] args) {
-        System.out.println("I'm version 7.1!");
-        System.out.println("I have a bug!");
+        System.out.println("I'm version 7.2!");
+        System.out.println("I'm version 7.2!");
+        System.out.println("I'm version 7.2!");
+        System.out.println("I'm version 7.2!");
+        System.out.println("I'm version 7.2!");
+        System.out.println("I'm version 7.2!");System.out.println("I have a bug!");
     }
 } 
