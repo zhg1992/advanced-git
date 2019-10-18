d2998016a8f207848bbc81f0af3b99159b2da193
Bo Zhang <zhangbodut@gmail.com>
2019-10-18 19:47:49 +0800

    Version 8.0

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 public class Product {
     public static void main(String[] args) {
-        System.out.println("I'm version 7.0!");
+        System.out.println("I'm version 8.0!");
         System.out.println("I have a bug!");
     }
 } 
