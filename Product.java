2420862d217df9fc732a38a67a2ae6b2e836322f
Bo Zhang <zhangbodut@gmail.com>
2019-10-18 20:00:18 +0800

    Version 10.0

--- Product.java
+++ Product.java
@@ -1,7 +1,6 @@
 public class Product {
     public static void main(String[] args) {
-        System.out.println("I'm version 9.1!");
-        System.out.println("I have a bug!");
+        System.out.println("I'm version 10.0!");
         System.out.println("A bad feature!");
     }
 } 
