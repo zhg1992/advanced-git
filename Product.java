9dfb38fc7f9a33299801c695092de3e2e8b67607
Bo Zhang <zhangbodut@gmail.com>
2019-10-18 19:59:24 +0800

    A bad feature

--- Product.java
+++ Product.java
@@ -2,5 +2,6 @@ public class Product {
     public static void main(String[] args) {
         System.out.println("I'm version 8.0!");
         System.out.println("I have a bug!");
+        System.out.println("A bad feature!");
     }
 } 
