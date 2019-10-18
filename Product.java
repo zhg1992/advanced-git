d3c84fe32615597c06b2fa2f2e2768f6ca979195
Bo Zhang <zhangbodut@gmail.com>
2019-10-18 19:45:59 +0800

    Version 6.0

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 public class Product {
     public static void main(String[] args) {
-        System.out.println("I'm version 5.0!");
+        System.out.println("I'm version 6.0!");
         System.out.println("I have a bug!");
     }
 } 
